5f0a461215a9b7d078989405627be6eb373271f4
Ekaterina_Kostyra <60934863+EkaterinaKostyra@users.noreply.github.com>
2020-02-24 21:28:48 +0300

    Add files via upload

---
+++ table.py
@@ -0,0 +1,11 @@
+pH=float(input('Введите pH: '))
+if pH==6.94:
+    print('Li')
+elif pH==54.938:
+    print('Mn')
+elif pH==200.59:
+    print('Hg')
+elif pH==35.45:
+    print('Cl')
+else:
+    print('Что это?')
