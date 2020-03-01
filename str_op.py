bb257e438b1f344edbda9a09faacb70dba41eee1
Ekaterina_Kostyra <60934863+EkaterinaKostyra@users.noreply.github.com>
2020-03-01 15:53:45 +0300

    Add files via upload

---
+++ str_op.py
@@ -0,0 +1,20 @@
+s = 'У Лукоморья 123 Дуб Зеленый 456'
+if 'я' in s:
+    print (s[10])
+if s!='' :
+    print(True)
+else:
+    print (s.upper())
+
+len(s)
+if len(s)>int(4):
+    print(s.lower())
+        
+t=list(s)
+t[0:1]='O'
+x=''.join(t)
+print(x)
+
+
+
+    
