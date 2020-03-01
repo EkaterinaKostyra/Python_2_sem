e8cd7d4e8b2aa2494befa643719f9745be5bd729
Ekaterina_Kostyra <60934863+EkaterinaKostyra@users.noreply.github.com>
2020-03-01 16:07:16 +0300

    Add files via upload

---
+++ if_random_game.py
@@ -0,0 +1,9 @@
+x=int(input('ведите целое число от 1 до 10: '))
+def game(x):
+    import random
+    return random.randit(1,4)
+if 1<=x<=4:
+    print('Победа!')
+else:
+    print('Попробуйте ещё раз') 
+         
