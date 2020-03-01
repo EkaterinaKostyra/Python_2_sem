91350e90d53f8443b568bd193819f8b57d6b0da1
Ekaterina_Kostyra <60934863+EkaterinaKostyra@users.noreply.github.com>
2020-03-01 16:47:24 +0300

    Add files via upload

--- if_random_game.py
+++ if_random_game.py
@@ -2,8 +2,8 @@ x=int(input('ведите целое число от 1 до 10: '))
 def game(x):
     import random
     return random.randit(1,4)
-if 1<=x<=4:
+if 1<=x<= 4:
     print('Победа!')
 else:
     print('Попробуйте ещё раз') 
-         
+#https://drakonhub.com/ide/doc/ekas101/2
